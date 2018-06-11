d590a0a65d65e2b677f3e5482d46232d1aafdf05
Marviel <lukebechtel4@gmail.com>
2018-06-11 18:25:22 -0400

    move to modular rendering

--- js/main.js
+++ js/main.js
@@ -1,119 +1,101 @@
-// standard global variables
-var container, scene, camera, renderer;
+class VisualisaRoot {
+  constructor(){
+    // standard global variables
 
-// SCENE
-scene = new THREE.Scene();
-
-// CAMERA
-camera = new THREE.PerspectiveCamera(45, 1, 0.1, 10000);
-scene.add( camera );
-camera.position.set(300,300,-300);
-camera.lookAt( scene.position );
-
-// RENDERER
-renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
-renderer.setClearColor( 0xffffff, 0 );
-renderer.setPixelRatio( window.devicePixelRatio );
-renderer.setSize( window.innerWidth, window.innerHeight );
-
-container = document.body;
+    // SCENE
+    this.scene = new THREE.Scene();
 
 
-scene.background = new THREE.Color(0x000000);
-var light = new THREE.PointLight( 0xff2200 );
-light.position.set( 100, 100, 100 );
-scene.add( light );
-var light = new THREE.AmbientLight( 0x666666);
-scene.add( light );
-var geometry = new THREE.BoxGeometry( 100, 100, 100 );
-var material = new THREE.MeshPhongMaterial( { color: 0xffffff, morphTargets: true } );
-// construct 8 blend shapes
-for ( var i = 0; i < 8; i ++ ) {
-  var vertices = [];
-  for ( var v = 0; v < geometry.vertices.length; v ++ ) {
-  vertices.push( geometry.vertices[ v ].clone() );
-  if ( v === i ) {
-  vertices[ vertices.length - 1 ].x *= 4;
-  vertices[ vertices.length - 1 ].y *= 4;
-  vertices[ vertices.length - 1 ].z *= 4;
+    // CAMERA
+    this.camera = new THREE.PerspectiveCamera(45, 1, 0.1, 10000);
+    this.scene.add( this.camera );
+    this.camera.position.set(300,300,-300);
+    this.camera.lookAt( this.scene.position );
+
+    // RENDERER
+    this.renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
+    this.renderer.setClearColor( 0xffffff, 0 );
+    this.renderer.setPixelRatio( window.devicePixelRatio );
+    this.renderer.setSize( window.innerWidth, window.innerHeight );
+
+    this.container = document.body;
+
+
+    this.scene.background = new THREE.Color(0x000000);
+    var light = new THREE.PointLight( 0xff2200 );
+    light.position.set( 100, 100, 100 );
+    this.scene.add( light );
+    var light = new THREE.AmbientLight( 0x666666);
+    this.scene.add( light );
+
+    this.controls = new THREE.OrbitControls( this.camera, this.renderer.domElement );
+    this.controls.autoRotate = true;
+
+    navigator.getUserMedia  = navigator.getUserMedia ||
+                            navigator.webkitGetUserMedia ||
+                            navigator.mozGetUserMedia ||
+                            navigator.msGetUserMedia;
+
+
+
+    this.changeMode = (newMode) => {
+      this.mode.modeExit(this);
+      newMode.modeEnter(this);
+      this.mode = newMode;
+    }
+
+    this.start_mic = () => {
+      if (navigator.getUserMedia) {
+        var el = document.getElementById('enable-microphone-button');
+        el.remove();
+
+
+        this.container.appendChild( this.renderer.domElement );
+        navigator.getUserMedia({ audio: true, video: false }, (stream) => {
+          this.audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+          this.analyser = this.audioCtx.createAnalyser();
+          this.source = this.audioCtx.createMediaStreamSource( stream );
+
+          this.source.connect(this.analyser);
+
+          this.analyser.fftSize = 2048;
+          this.bufferLength = this.analyser.frequencyBinCount;
+          this.dataArray = new Uint8Array( this.bufferLength );
+
+          this.animate();
+        }, function(){});
+      } else {
+        // fallback.
+      }
+    }
+
+    this.animate = () => {
+      requestAnimationFrame( this.animate );
+
+      this.analyser.getByteFrequencyData(this.dataArray);
+
+      // Use the current mode to animate...
+      this.mode.modeAnimate(this, this.analyser, this.dataArray);
+
+      // Update objects that care...
+      this.controls.update();
+
+      this.render();
+    }
+
+    this.render = ()=>{
+      this.renderer.render( this.scene, this.camera );
+    }
+
+    // Set mode..
+    this.mode = new CubeMode(8, 255);
+    this.mode.modeEnter(this);
+
+    // Render.
+    this.render();
   }
-  }
-  geometry.morphTargets.push( { name: "target" + i, vertices: vertices } );
-}
-mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
-
-controls = new THREE.OrbitControls( camera, renderer.domElement );
-controls.autoRotate = true;
 
 
-mesh = new THREE.Mesh(
-  geometry,
-  material
-);
-
-threeobj = new THREE.Object3D();
-threeobj.add( mesh );
-
-scene.add( threeobj );
-
-navigator.getUserMedia  = navigator.getUserMedia ||
-                        navigator.webkitGetUserMedia ||
-                        navigator.mozGetUserMedia ||
-                        navigator.msGetUserMedia;
-
-var audioCtx;
-var analyser;
-var source;
-var bufferLength;
-var dataArray;
-
-function animate(){
-  requestAnimationFrame( animate );
-
-  analyser.getByteFrequencyData(dataArray);
-  var binCount = analyser.frequencyBinCount;
-
-  for(var i = 0; i < 8; i++) {
-    var dataArrI = Math.floor(i*(binCount/8)); // Evenly spaced over frequencies.
-    //var dataArrI(i + 10) *2
-    //material.uniforms.amplitude.value[i] = -(dataArray[(i + 10) * 2] / 255) + 1;
-    //material.color.r = (dataArray[dataArrI]/255);
-    mesh.morphTargetInfluences[ i ] = dataArray[dataArrI]/255;
-  };
-  material.color.r = (dataArray[0]/255)
-  material.reflectivity = (dataArray[0]/255)
-  controls.update();
-
-  render();
 }
 
-function render(){
-  renderer.render( scene, camera );
-}
-
-render();
-
-function start_mic(){
-  if (navigator.getUserMedia) {
-  var el = document.getElementById('enable-microphone-button');
-  el.remove(); // Removes the div with the 'div-02' id
-
-  container.appendChild( renderer.domElement );
-    navigator.getUserMedia({ audio: true, video: false }, function( stream ) {
-      audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-      analyser = audioCtx.createAnalyser();
-      source = audioCtx.createMediaStreamSource( stream );
-
-      source.connect(analyser);
-
-      analyser.fftSize = 2048;
-      bufferLength = analyser.frequencyBinCount;
-      dataArray = new Uint8Array( bufferLength );
-
-      animate();
-    }, function(){});
-  } else {
-    // fallback.
-  }
-}
+const vRoot = new VisualisaRoot();
